351f4e400d9a5e2c18781c902255cd0b4ff0dc31
fatezhou@163.com <fatezhou@163.com>
2018-12-21 23:02:49 +0800

    sln

--- DirChecker/Config.cpp
+++ DirChecker/Config.cpp
@@ -1,4 +1,5 @@
 #include "Config.h"
+#include <windows.h>
 
 
 Config::Config()
@@ -8,4 +9,76 @@ Config::Config()
 
 Config::~Config()
 {
+}
+
+void Config::Load()
+{
+	std::string strPath = ::GetDir() + "\\config.ini";
+	char sz[1024] = "";
+	GetPrivateProfileStringA("set", "user", "81758624@qq.com", sz, 1024, strPath.c_str());
+	strUser = sz;
+
+	GetPrivateProfileStringA("set", "password", "hoqaduzkavthbhcb", sz, 1024, strPath.c_str());
+	strPassword = sz;
+
+	GetPrivateProfileStringA("set", "dir", ".\\", sz, 1024, strPath.c_str());
+	strDir = sz;
+
+	GetPrivateProfileStringA("set", "emails", "", sz, 1024, strPath.c_str());
+	strEmail = sz;
+
+	GetPrivateProfileStringA("set", "fliter", "", sz, 1024, strPath.c_str());
+	strFilterDir = sz;
+}
+
+std::string Config::GetUser()
+{
+	return strUser;
+}
+
+std::string Config::GetPassword()
+{
+	return strPassword;
+}
+
+std::string Config::GetDir()
+{
+	return strDir;
+}
+
+vector<std::string> Config::GetEmails()
+{
+	return Split(strEmail, ";");
+}
+
+vector<std::string> Config::GetFliter()
+{
+	return Split(strFilterDir, ";");
+}
+
+std::string GetDir()
+{
+	static std::string strPath;
+	if (strPath.empty()){
+		char sz[1024] = "";
+		GetModuleFileNameA(NULL, sz, 1024);
+		char* ptr = strrchr(sz, '\\');
+		if (ptr){
+			*ptr = 0;
+		}
+		strPath = sz;
+	}
+	return strPath;
+}
+
+vector<std::string> Split(string strSrc, string strDeli)
+{
+	vector<std::string> vec;
+	int nPos = 0, nBegin = 0;
+	while (nPos != -1){
+		nPos = strSrc.find(strDeli, nBegin); 
+		vec.push_back(strSrc.substr(nBegin, nPos - nBegin));
+		nBegin = nPos + strDeli.length();
+	}
+	return vec;
 }
--- DirChecker/Config.h
+++ DirChecker/Config.h
@@ -1,4 +1,13 @@
 #pragma once
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+std::string GetDir();
+vector<std::string> Split(string strSrc, string strDeli);
+
 class Config
 {
 public:
@@ -6,6 +15,13 @@ public:
 	~Config();
 
 	void Load();
+	std::string GetUser();
+	std::string GetPassword();
+	std::string GetDir();
+	vector<std::string> GetEmails();
+	vector<std::string> GetFliter();
 
+private:
+	std::string strUser, strPassword, strDir, strEmail, strFilterDir;
 };
 
